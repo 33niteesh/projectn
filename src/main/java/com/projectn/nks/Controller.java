d1dea01a5ec31ea95c59c6bea977be1f062c7484
33niteesh <niteeshsatyapu33@gmail.com>
2024-10-21 16:23:46 +0530

    Update Controller.java

--- src/main/java/com/projectn/nks/Controller.java
+++ src/main/java/com/projectn/nks/Controller.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -22,4 +25,8 @@ public class Controller {
         return entity;
     }
 
+    @PutMapping("path/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        return entity;
+    }
 }
