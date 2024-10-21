379f7440818f1e06ba36c69a46e94a0cdd600d84
33niteesh <niteeshsatyapu33@gmail.com>
2024-10-21 16:07:56 +0530

    Update Controller.java

--- src/main/java/com/projectn/nks/Controller.java
+++ src/main/java/com/projectn/nks/Controller.java
@@ -4,15 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
     @GetMapping("/get")
-    public String getMethodName(@RequestParam String param) {
-        
+    public String getMethodName(@RequestParam String param) {   
         return "Hello, " + param+" How are you ";
+    }
+
+    @PostMapping("/post")
+    public String postMethodName(@RequestBody String entity) {
+        return entity;
     }
 
 }
