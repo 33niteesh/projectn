889b49a2236f0de41a50ab069d0e59ea0e663850
33niteesh <niteeshsatyapu33@gmail.com>
2024-10-21 14:24:02 +0530

    set up springboot app  controller

---
+++ src/main/java/com/projectn/nks/NksApplication.java
@@ -0,0 +1,13 @@
+package com.projectn.nks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NksApplication.class, args);
+	}
+
+}
